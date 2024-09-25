02b3ec37a319213db0c4ec72cb2678caae9136ce
martinrhan <martinrhan@126.com>
2024-09-25 16:43:50 +0800

    Add project files.

---
+++ SingletonSourceGenerator.Attributes/SingletonAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SingletonSourceGenerator.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+[System.Diagnostics.Conditional("DEBUG")]
+public class SingletonAttribute : Attribute {
+
+}
+
---
+++ SingletonSourceGenerator/SingletonGenerator.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis;
+using System;
+
+[Generator]
+public class SingletonGenerator : IIncrementalGenerator {
+    public void Initialize(IncrementalGeneratorInitializationContext context) {
+        var provider_typeSymbols =
+            context.SyntaxProvider
+                .ForAttributeWithMetadataName("SingletonSourceGenerator.Attributes.SingletonAttribute", (_, _) => true,
+                    (context, token) => {
+                        var symbol = context.SemanticModel.GetDeclaredSymbol(context.TargetNode, token);
+                        var ts = symbol as ITypeSymbol;
+                        return ts;
+                    }
+                );
+
+        context.RegisterSourceOutput(provider_typeSymbols, static (context, ts) => {
+            if (ts == null) return;
+            string code = $$"""
+            #nullable enable
+            namespace {{ts.ContainingNamespace}};
+            public partial class {{ts.Name}}{
+                private static {{ts.Name}}? instance;
+                public static {{ts.Name}} Instance => instance ??= new();
+            }
+            """;
+            context.AddSource($"{ts.Name}.Singleton.cs", code);
+        });
+    }
+}
